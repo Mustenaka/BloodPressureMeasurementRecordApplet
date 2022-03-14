0a15975295a8fd1d03ef05ccf9c8687a9f29f668
mustenaka <mumten120@outlook.com>
2022-03-14 14:56:57 +0800

    Update

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func RunProgram() {
 	user := model.BaseUser{
 		// UserId:     11,
 		OpenId:     tools.RandomUpperString(16),
-		UserName:   "王五",
-		Tel:        "18278266123",
-		Email:      "wangwu@outlook.com",
+		UserName:   "刘头",
+		Tel:        "18278262188",
+		Email:      "liutou@outlook.com",
 		Permission: 3,
 		LastTime:   time.Now().Format("2006-01-02 15:04:05"),
 		Sex:        "男",
@@ -29,32 +29,34 @@ func RunProgram() {
 		fmt.Println("无法插入数据")
 	}
 
-	var users []model.BaseUser
-	if err := model.DB.Where(&model.BaseUser{UserName: "李翠花"}).Find(&users); err.Error != nil {
-		// 错误处理
-		fmt.Println("没有找到该数据333")
-	}
+	// 查询
+	// var users []model.BaseUser
+	// if err := model.DB.Where(&model.BaseUser{UserName: "李翠花"}).Find(&users); err.Error != nil {
+	// 	// 错误处理
+	// 	fmt.Println("没有找到该数据333")
+	// }
 
 	// 删除
 	// model.DB.Where(&model.BaseUser{UserName: "翠花"}).Delete(&model.BaseUser{})
 	// 修改
 	// model.DB.Model(&model.BaseUser{}).Where(&model.BaseUser{UserName: "李翠花"}).Updates(model.BaseUser{Sex: "其他"})
 
-	for _, user := range users {
-		fmt.Println(user)
+	// 通过查询的用户插入高血压数据
+	// for _, user := range users {
+	// 	fmt.Println(user)
 
-		// bpRecord := model.PatientBpRecord{
-		// 	UserId:       user.UserId,
-		// 	RecordDate:   time.Now().Format("2006-01-02"),
-		// 	RecordTime:   time.Now().Format("15:04:05"),
-		// 	LowPressure:  int16(tools.RandomInt(60, 90)),
-		// 	HighPressure: int16(tools.RandomInt(90, 150)),
-		// }
-		// if err := model.DB.Create(&bpRecord); err.Error != nil {
-		// 	// 错误处理
-		// 	fmt.Println("无法插入数据")
-		// }
-	}
+	// 	// bpRecord := model.PatientBpRecord{
+	// 	// 	UserId:       user.UserId,
+	// 	// 	RecordDate:   time.Now().Format("2006-01-02"),
+	// 	// 	RecordTime:   time.Now().Format("15:04:05"),
+	// 	// 	LowPressure:  int16(tools.RandomInt(60, 90)),
+	// 	// 	HighPressure: int16(tools.RandomInt(90, 150)),
+	// 	// }
+	// 	// if err := model.DB.Create(&bpRecord); err.Error != nil {
+	// 	// 	// 错误处理
+	// 	// 	fmt.Println("无法插入数据")
+	// 	// }
+	// }
 
 }
 
