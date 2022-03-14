b7ec0e3d978b93f740c4957ec9cfa17d21f51175
mustenaka <mumten120@outlook.com>
2022-03-14 11:48:09 +0800

    更新工具类

--- tools/tools.go
+++ tools/tools.go
@@ -5,22 +5,39 @@ import (
 	"time"
 )
 
-// Returns an int >= min, < max
-func randomInt(min, max int) int {
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// 生成随机数字Returns an int >= min, < max
+func RandomInt(min, max int) int {
 	return min + rand.Intn(max-min)
 }
 
-// 生成随机字符串
-func RandomString(len int) string {
-	rand.Seed(time.Now().UnixNano())
+// 生成随机大写字符串
+func RandomUpperString(len int) string {
 	// len 不能低于 0
 	if len <= 0 {
 		return ""
 	}
-	// 生成随机字符串
+	// 生成随机字符串-大写
 	bytes := make([]byte, len)
 	for i := 0; i < len; i++ {
-		bytes[i] = byte(randomInt(65, 90))
+		bytes[i] = byte(RandomInt(65, 90))
+	}
+	return string(bytes)
+}
+
+// 生成随机小写字符串
+func RandomLowerString(len int) string {
+	// len 不能低于 0
+	if len <= 0 {
+		return ""
+	}
+	// 生成随机字符串-大写
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(RandomInt(97, 122))
 	}
 	return string(bytes)
 }
