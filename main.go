acb12293b70ef724cc39f1ecbe2f5376df316835
mustenaka <mumten120@outlook.com>
2022-03-14 11:48:27 +0800

    完成数据库从用户到血压记录的设置

--- main.go
+++ main.go
@@ -13,18 +13,31 @@ import (
 
 // Test hello
 func RunProgram() {
-	user := model.BaseUser{
-		// UserId:     11,
-		OpenId:     tools.RandomString(16),
-		UserName:   "李翠莲",
-		Tel:        "18778262136",
-		Email:      "leecuilian@outlook.com",
-		Permission: 3,
-		LastTime:   time.Now().Format("2006-01-02 15:04:05"),
-		Sex:        "女",
-		Status:     "关闭",
+	// user := model.BaseUser{
+	// 	// UserId:     11,
+	// 	OpenId:     tools.RandomString(16),
+	// 	UserName:   "李翠莲",
+	// 	Tel:        "18778262136",
+	// 	Email:      "leecuilian@outlook.com",
+	// 	Permission: 3,
+	// 	LastTime:   time.Now().Format("2006-01-02 15:04:05"),
+	// 	Sex:        "女",
+	// 	Status:     "关闭",
+	// }
+
+	var users []model.BaseUser
+	model.DB.Where(&model.BaseUser{UserName: "李翠莲"}).Find(&users)
+
+	fmt.Println(users[0])
+
+	bpRecord := model.PatientBpRecord{
+		UserId:       users[0].UserId,
+		RecordDate:   time.Now().Format("2006-01-02"),
+		RecordTime:   time.Now().Format("15:04:05"),
+		LowPressure:  int16(tools.RandomInt(60, 90)),
+		HighPressure: int16(tools.RandomInt(90, 150)),
 	}
-	model.DB.Create(&user)
+	model.DB.Create(&bpRecord)
 }
 
 func main() {
