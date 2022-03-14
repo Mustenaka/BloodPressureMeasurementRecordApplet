137f753a13d6c507d3061c10912c1b80df7fecfb
mustenaka <mumten120@outlook.com>
2022-03-14 11:20:36 +0800

    完成用户血压记录表格

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ func RunProgram() {
 	user := model.BaseUser{
 		// UserId:     11,
 		OpenId:     tools.RandomString(16),
-		UserName:   "李翠花",
-		Tel:        "18778262137",
-		Email:      "leecuihua@outlook.com",
+		UserName:   "李翠莲",
+		Tel:        "18778262136",
+		Email:      "leecuilian@outlook.com",
 		Permission: 3,
 		LastTime:   time.Now().Format("2006-01-02 15:04:05"),
 		Sex:        "女",
-		Status:     "开启",
+		Status:     "关闭",
 	}
 	model.DB.Create(&user)
 }
