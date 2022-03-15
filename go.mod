064a0b4a638ccdf5088713c93acfcbc272bdfb02
mustenaka <mumten120@outlook.com>
2022-03-15 16:27:22 +0800

    完成logger的日志测试

--- go.mod
+++ go.mod
@@ -12,13 +12,16 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/spf13/viper v1.10.1 // indirect
 	github.com/ugorji/go v1.2.7 // indirect
 	github.com/widuu/goini v0.0.0-20180603013956-56a38bd2e09b
 	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
+	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20220314234724-5d542ad81a58 // indirect
 	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/mysql v1.3.2
 	gorm.io/gorm v1.23.2
---
+++ pkg/log/log_test.go
@@ -0,0 +1,48 @@
+package log
+
+import (
+	"BloodPressure/pkg/global"
+	"BloodPressure/pkg/log/constant"
+	"BloodPressure/tools/uuid"
+	"context"
+	"testing"
+)
+
+func init() {
+	defer Sync()
+	conf := global.GetInstance()
+	logConfig := LogConfig{
+		Level:      conf.GetConfigValue("logconfig", "level"),
+		FileName:   conf.GetConfigValue("logconfig", "file-name"),
+		TimeFormat: constant.TimeLayout,
+		MaxSize:    1,
+		MaxBackups: 5,
+		MaxAge:     2,
+		Compress:   false,
+		LocalTime:  true,
+		Console:    true,
+	}
+	InitLogger(&(logConfig), conf.GetConfigValue("basicinfo", "appName"))
+}
+
+func TestInfo(t *testing.T) {
+	Info("test info", WithPair("age", 20), WithPair("name", "小明"))
+}
+
+func BenchmarkInfo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Info("test info", WithPair("age", 20), WithPair("name", "小明"))
+	}
+}
+
+func TestTempLogger_Debug(t *testing.T) {
+	c := context.WithValue(context.TODO(), constant.RequestId, uuid.GenUUID16())
+	RID(c).Debug("test log Request ID", WithPair("age", 20), WithPair("name", "小明"))
+	// 在包外使用时, 可以把web框架比如*gin.Context实例直接传入
+	// log.RID(c).Debug("test log Request ID", WithPair("age", 20), WithPair("name", "小明"))
+}
+
+func TestTempLogger_Debugf(t *testing.T) {
+	c := context.WithValue(context.TODO(), constant.RequestId, uuid.GenUUID16())
+	RID(c).Debugf("age=%d,name=%s\r\n", 20, "小明")
+}
