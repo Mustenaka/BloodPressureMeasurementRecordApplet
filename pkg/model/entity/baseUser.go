ac82f73916fa41dab461d308c5dcf49c125880e9
mustenaka <mumten120@outlook.com>
2022-03-16 11:50:53 +0800

    添加用户基类字段

--- pkg/model/entity/baseUser.go
+++ pkg/model/entity/baseUser.go
@@ -9,6 +9,8 @@ type BaseUser struct {
 	Email      string // 用户邮件
 	Permission int    // 用户权限
 	LastTime   string // 用户的最后一次上线时间
+	CreateTime string // 该账户的注册时间
+	Birthday   string // 该账户的生日，用来确定年龄
 	Sex        string // 用户性别，可选项"男"，"女"，"其他"
 	Status     string // 用户状态，可选项"开启"，"关闭"
 }
@@ -17,5 +19,5 @@ type BaseUser struct {
 type PasswordUser struct {
 	AdminId  uint   // 管理权限id
 	UserId   uint   // 用户id
-	Password string // 用户密码(需要加密)
+	Password string // 用户密码(需要加密保存)
 }
