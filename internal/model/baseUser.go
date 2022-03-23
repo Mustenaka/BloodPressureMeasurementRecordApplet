827cdd2d2743d53e0621e268467173d8189fc299
mustenaka <mumten120@outlook.com>
2022-03-23 23:31:52 +0800

    删除独立的用户密码表，并合并到基础用户中

--- internal/model/baseUser.go
+++ internal/model/baseUser.go
@@ -5,6 +5,7 @@ type BaseUser struct {
 	UserId     uint   // 用户id
 	OpenId     string // 用户的微信openid
 	UserName   string // 用户名称-真实姓名
+	Password   string // 用户密码(需要加密保存)
 	Tel        string // 用户手机号
 	Email      string // 用户邮件
 	Permission int    // 用户权限
