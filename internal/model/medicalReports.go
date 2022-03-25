fd61d0d8803b51e7282396f78ade4aa3be6c62ac
mustenaka <mumten120@outlook.com>
2022-03-25 11:46:13 +0800

    【添加】实体对象命名标准修改，并添加tableName和验证模块

--- internal/model/medicalReports.go
+++ internal/model/medicalReports.go
@@ -1,7 +1,20 @@
 package model
 
+import validator "gopkg.in/go-playground/validator.v9"
+
 // 体检报告表
 type MedicalReports struct {
 	Id     uint // 检查报告id
 	UserId uint // 用户id
+}
+
+// 获取表名称
+func (MedicalReports) TableName() string {
+	return "medical_reports"
+}
+
+// 判断有效性
+func (medicalReports *MedicalReports) Validate() error {
+	validate := validator.New()
+	return validate.Struct(medicalReports)
 }
---
+++ internal/model/patientBpRecord.go
@@ -0,0 +1,24 @@
+package model
+
+import validator "gopkg.in/go-playground/validator.v9"
+
+// 血压记录表
+type PatientBpRecord struct {
+	RecordId     uint   // 血压记录id
+	UserId       uint   // 用户id
+	RecordDate   string // 血压记录日期
+	RecordTime   string // 血压记录时间
+	LowPressure  int16  // 低压
+	HighPressure int16  // 高压
+}
+
+// 获取表名称
+func (PatientBpRecord) TableName() string {
+	return "patient_bp_records"
+}
+
+// 判断有效性
+func (patientBpRecord *PatientBpRecord) Validate() error {
+	validate := validator.New()
+	return validate.Struct(patientBpRecord)
+}
