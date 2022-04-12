31741cb329f0164c1d6cc15d6c4a95b085099d8e
mustenaka <mumten120@outlook.com>
2022-04-12 22:04:12 +0800

    更新model: 基础用户 baseUser

--- internal/model/baseUser.go
+++ internal/model/baseUser.go
@@ -13,7 +13,6 @@ type BaseUser struct {
 	Password   string // 用户密码(需要加密保存)
 	Tel        string // 用户手机号
 	Email      string // 用户邮件
-	Permission int    // 用户权限
 	LastTime   string // 用户的最后一次上线时间
 	CreateTime string // 该账户的注册时间
 	Birthday   string // 该账户的生日，用来确定年龄
