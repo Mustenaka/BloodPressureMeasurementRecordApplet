8a9f746e99af211d3b2b8493c16aba3cc3b2223b
mustenaka <mumten120@outlook.com>
2022-04-14 16:44:15 +0800

    代码命名修正

--- internal/service/patientBpRecord_service.go
+++ internal/service/patientBpRecord_service.go
@@ -19,32 +19,32 @@ type PatientBpRecordService interface {
 
 // patientBpRecordService 实现UserService接口
 type patientBpRecordService struct {
-	ur repo.PatientBpRecordRepo
+	bprr repo.PatientBpRecordRepo
 }
 
 // 新血压记录服务
 func NewPBPRecordService(_ur repo.PatientBpRecordRepo) *patientBpRecordService {
 	return &patientBpRecordService{
-		ur: _ur,
+		bprr: _ur,
 	}
 }
 
 // 获取全部的测量数据
 func (us *patientBpRecordService) GetById(ctx context.Context, uid uint) ([]model.PatientBpRecord, error) {
-	return us.ur.GetRecordById(ctx, uid)
+	return us.bprr.GetRecordById(ctx, uid)
 }
 
 // 根据限制天数获取数据
 func (us *patientBpRecordService) GetByIdLimitDay(ctx context.Context, uid uint, limitdays int) ([]model.PatientBpRecord, error) {
 	// 如果限制天数为0，则降级为获取全部数据
 	if limitdays == 0 {
-		return us.ur.GetRecordById(ctx, uid)
+		return us.bprr.GetRecordById(ctx, uid)
 	}
-	return us.ur.GetRecordByIdLimitDays(ctx, uid, limitdays)
+	return us.bprr.GetRecordByIdLimitDays(ctx, uid, limitdays)
 }
 
 // 添加一条血压记录
 func (us *patientBpRecordService) AddById(ctx context.Context, uid uint, low, high, heartRate int) error {
 	// log.Debug("添加血压记录信息", log.WithPair("uid", uid), log.WithPair("low", low), log.WithPair("high", high))
-	return us.ur.AddRecord(ctx, uid, low, high, heartRate)
+	return us.bprr.AddRecord(ctx, uid, low, high, heartRate)
 }
