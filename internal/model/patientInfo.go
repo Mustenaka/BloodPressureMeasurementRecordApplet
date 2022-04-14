402e5d862b2f2970d13e554ea3e0e78ae65d4c77
mustenaka <mumten120@outlook.com>
2022-04-14 16:52:42 +0800

    【修改】model 之前拆分了，现在合并回去

--- internal/model/patientInfo.go
+++ internal/model/patientInfo.go
@@ -4,17 +4,15 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
-// 全部信息
-type PatientAllInfo struct {
-	PatientInfo     // patient_infos 表中全部信息
-	PatientPlusInfo // base_users 表中部分需要的信息
-}
-
 // 患者病历信息
 type PatientInfo struct {
 	// patient_infos 内容
 	PatientId                 uint   // 病历信息
 	UserId                    uint   // 对应用户
+	RealName                  string //真实姓名
+	Sex                       string // 性别
+	Birthday                  string // 生日
+	Tel                       string // 电话号码
 	IsMarried                 bool   // 0-未婚、1-已婚
 	HbpYears                  int    // 高血压患病时间（年）
 	Anamnesis                 string // 既往病史(对应表格1~12)
@@ -33,16 +31,6 @@ type PatientInfo struct {
 	AntihypertensivePlan      string // 降压方案
 	IsNondrugControlPlan      bool   // 是否非药物控制手段
 	NondrugControlPlan        string // 非药物控制手段内容
-
-}
-
-// 附加内容
-type PatientPlusInfo struct {
-	// base_users 内容 - 姓名、性别、生日、电话、邮箱
-	RealName string
-	Sex      string
-	Birthday string
-	Tel      string
 }
 
 // 获取表名称
@@ -54,10 +42,4 @@ func (PatientInfo) TableName() string {
 func (patientInfo *PatientInfo) Validate() error {
 	validate := validator.New()
 	return validate.Struct(patientInfo)
-}
-
-// 判断有效性
-func (patientAllInfo *PatientAllInfo) Validate() error {
-	validate := validator.New()
-	return validate.Struct(patientAllInfo)
 }
