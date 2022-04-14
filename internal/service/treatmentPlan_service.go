f0734999b14f928ba47475585448b2ba1a0b4517
mustenaka <mumten120@outlook.com>
2022-04-14 16:41:52 +0800

    【名称修正】治疗方案 ur改成tr（user repo -> treatment repo）

--- internal/service/treatmentPlan_service.go
+++ internal/service/treatmentPlan_service.go
@@ -19,31 +19,31 @@ type TreatmentPlanService interface {
 
 // treatmentPlanService 实现UserService接口
 type treatmentPlanService struct {
-	ur repo.TreatmentPlanRepo
+	tr repo.TreatmentPlanRepo
 }
 
 // 新血压记录服务
 func NewTreatmentPlanService(_ur repo.TreatmentPlanRepo) *treatmentPlanService {
 	return &treatmentPlanService{
-		ur: _ur,
+		tr: _ur,
 	}
 }
 
 // 获取全部的测量数据
 func (us *treatmentPlanService) GetById(ctx context.Context, uid uint) ([]model.TreatmentPlan, error) {
-	return us.ur.GetPlanById(ctx, uid)
+	return us.tr.GetPlanById(ctx, uid)
 }
 
 // 根据限制天数获取数据
 func (us *treatmentPlanService) GetByIdLimit(ctx context.Context, uid uint, limit int) ([]model.TreatmentPlan, error) {
 	// 如果限制天数为0，则降级为获取全部数据
 	if limit == 0 {
-		return us.ur.GetPlanById(ctx, uid)
+		return us.tr.GetPlanById(ctx, uid)
 	}
-	return us.ur.GetPlanByIdLimit(ctx, uid, limit)
+	return us.tr.GetPlanByIdLimit(ctx, uid, limit)
 }
 
 // 添加一条血压记录
 func (us *treatmentPlanService) AddById(ctx context.Context, uid uint, plan, note string) error {
-	return us.ur.AddPlan(ctx, uid, plan, note)
+	return us.tr.AddPlan(ctx, uid, plan, note)
 }
