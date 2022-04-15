6c9410fab64a88a232969624c23772fd0461ed85
mustenaka <mumten120@outlook.com>
2022-04-15 18:51:38 +0800

    【修改】model.baseUser每新增别名

--- internal/model/baseUser.go
+++ internal/model/baseUser.go
@@ -6,18 +6,18 @@ import (
 
 // 基础用户信息表
 type BaseUser struct {
-	UserId     uint   // 用户id
-	OpenId     string // 用户的微信openid
-	UserName   string // 用户名称-真实姓名
-	RealName   string // 用户真实姓名
-	Password   string // 用户密码(需要加密保存)
-	Tel        string // 用户手机号
-	Email      string // 用户邮件
-	LastTime   string // 用户的最后一次上线时间
-	CreateTime string // 该账户的注册时间
-	Birthday   string // 该账户的生日，用来确定年龄
-	Sex        string // 用户性别，可选项"男"，"女"，"其他"
-	Status     string // 用户状态，可选项"开启"，"关闭"
+	UserId     uint   `json:"user_id"`     // 用户id
+	OpenId     string `json:"open_id"`     // 用户的微信openid
+	UserName   string `json:"user_name"`   // 用户名称-真实姓名
+	RealName   string `json:"real_name"`   // 用户真实姓名
+	Password   string `json:"password"`    // 用户密码(需要加密保存)
+	Tel        string `json:"tel"`         // 用户手机号
+	Email      string `json:"email"`       // 用户邮件
+	LastTime   string `json:"last_time"`   // 用户的最后一次上线时间
+	CreateTime string `json:"create_time"` // 该账户的注册时间
+	Birthday   string `json:"birthday"`    // 该账户的生日，用来确定年龄
+	Sex        string `json:"sex"`         // 用户性别，可选项"男"，"女"，"其他"
+	Status     string `json:"status"`      // 用户状态，可选项"开启"，"关闭"
 }
 
 // 获取表名称
