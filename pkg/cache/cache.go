3e1f5fcca288da951f7fc897ff0e6db7d5fe6103
mustenaka <mumten120@outlook.com>
2022-04-23 17:15:12 +0800

    【新增】pkg中添加redis的缓存模块

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -1,3 +1,36 @@
 package chache
 
-// 这个模块准备用来使用redis做高速缓存的，目前还没用到
+import (
+	"BloodPressure/pkg/config"
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var redisClient *redis.Client
+
+// 初始化redisClient
+func InitRedis(config config.Config) {
+	redisCfg := config.RedisConfig
+	redisClient = redis.NewClient(&redis.Options{
+		DB:           redisCfg.Db,
+		Addr:         redisCfg.Addr,
+		Password:     redisCfg.Password,
+		PoolSize:     redisCfg.PoolSize,
+		MinIdleConns: redisCfg.MinIdleConns,
+		IdleTimeout:  time.Duration(redisCfg.IdleTimeout) * time.Second,
+	})
+	_, err := redisClient.Ping(context.TODO()).Result()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetRedisClient() *redis.Client {
+	return redisClient
+}
+
+func CloseRedis() {
+	_ = redisClient.Close()
+}
