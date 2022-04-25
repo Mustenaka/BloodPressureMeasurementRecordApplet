f88e8bf4e28f79e83ba9e3e3d197d642cf980c67
mustenaka <mumten120@outlook.com>
2022-04-25 12:33:26 +0800

    服务器版权信息展示接口更新

--- internal/handler/copyright/copyright.go
+++ internal/handler/copyright/copyright.go
@@ -1,22 +1,15 @@
 package copyright
 
 import (
-	"BloodPressure/pkg/log"
-	"BloodPressure/pkg/version"
-
-	"net/http"
+	"BloodPressure/pkg/copyright"
+	"BloodPressure/pkg/response"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Copyright ping服务器状态
+// Copyright 服务器版权信息展示状态
 func Copyright() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// goVersion := runtime.Version()
-		AppVersion := version.AppVersion
-
-		log.Debug("result", log.WithPair("result", AppVersion))
-
-		c.String(http.StatusOK, AppVersion)
+		response.JSON(c, nil, copyright.GetInstance().GetCopyright())
 	}
 }
