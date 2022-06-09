425af2361565e355e35faaafa513d7eb2ba3d7f2
mustenaka <mumten120@outlook.com>
2022-06-10 00:04:46 +0800

    【添加】舌苔脉象model结构

--- internal/model/TongueDetail.go
+++ internal/model/TongueDetail.go
@@ -1,1 +1,25 @@
 package model
+
+import (
+	validator "gopkg.in/go-playground/validator.v9"
+)
+
+type TongueDetail struct {
+	Id            uint   `json:"id"`
+	UserId        uint   `json:"user_id"`
+	Tongue        string `json:"tongue"`
+	TongueCoating string `json:"tongue_coating"`
+	Pulse         string `json:"pulse"`
+	CreateAt      string `json:"create_at"`
+}
+
+// 获取表名称
+func (TongueDetail) TableName() string {
+	return "tongue_detail"
+}
+
+// 判断有效性
+func (tongueDetail *TongueDetail) Validate() error {
+	validate := validator.New()
+	return validate.Struct(tongueDetail)
+}
