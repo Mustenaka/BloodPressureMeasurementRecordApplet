ab22d958cea4d60affa9fbba4380d26aaf219fb3
mustenaka <mumten120@outlook.com>
2022-06-10 17:26:50 +0800

    【添加】心超报告

---
+++ internal/model/medicalReportEchocardiographys.go
@@ -0,0 +1,26 @@
+package model
+
+import (
+	validator "gopkg.in/go-playground/validator.v9"
+)
+
+// 心超报告
+type MedicalReportEchocardiographys struct {
+	Id       uint   `json:"id"`
+	UserId   uint   `json:"user_id"`
+	Ef       int    `json:"ef"`
+	Lvidd    int    `json:"lvidd"`
+	Lvids    int    `json:"lvids"`
+	CreateAt string `json:"create_at"` // 报告时间
+}
+
+// 获取表名称
+func (MedicalReportEchocardiographys) TableName() string {
+	return "medical_report_echocardiographys"
+}
+
+// 判断有效性
+func (medicalReportEchocardiographys *MedicalReportEchocardiographys) Validate() error {
+	validate := validator.New()
+	return validate.Struct(medicalReportEchocardiographys)
+}
