e2b1122cf012a5a2a7c3d7111ddf811c425001a5
mustenaka <mumten120@outlook.com>
2022-06-10 12:32:18 +0800

    【新增】我的检查报告-24小时动态血压报告

---
+++ internal/model/medicalReport24Hoursbprs.go
@@ -0,0 +1,36 @@
+package model
+
+import (
+	validator "gopkg.in/go-playground/validator.v9"
+)
+
+type MedicalReport24hoursbprs struct {
+	Id        uint   `json:"id"`
+	UserId    uint   `json:"user_id"`
+	DayHigh   int    `json:"day_high"`
+	DayLow    int    `json:"day_low"`
+	NightHigh int    `json:"night_high"`
+	NightLow  int    `json:"night_low"`
+	CreateAt  string `json:"create_at"`
+	// 最大高血压
+	MaxHigh     int    `json:"max_high"`
+	MaxHighTime string `json:"max_high_time"`
+	MaxLow      int    `json:"max_low"`
+	MaxLowTime  string `json:"max_low_time"`
+	// 最低高血压
+	MinHigh     int    `json:"min_high"`
+	MinHighTime string `json:"min_high_time"`
+	MinLow      int    `json:"min_low"`
+	MinLowTime  string `json:"min_low_time"`
+}
+
+// 获取表名称
+func (MedicalReport24hoursbprs) TableName() string {
+	return "medical_report_24hoursbprs"
+}
+
+// 判断有效性
+func (medicalReport24hoursbprs *MedicalReport24hoursbprs) Validate() error {
+	validate := validator.New()
+	return validate.Struct(medicalReport24hoursbprs)
+}
